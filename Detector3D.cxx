e32600690935d7c432708a2efbe7670eba9c9dd8
Brad Sawatzky <brads@jlab.org>
2015-04-24 12:21:55 -0400

    Delete test box code in 3D view

--- Detector3D.cxx
+++ Detector3D.cxx
@@ -36,45 +36,6 @@ Detector3D::Detector3D()
       top = mgr->MakeBox("TOP",medium,600,300,300);
       mgr->SetTopVolume(top); 
 
-      
-  /*
-      TGeoBBox *box1 = new TGeoBBox ("Box1", 128,128,128);
-      TGeoTranslation bt(-200,0,0);
-      TGeoRotation br;
-      br.SetAngles(90,0,90,90,0,0);
-      TGeoCombiTrans *btr = new TGeoCombiTrans (bt,br);
-      TGeoVolume *Box1 = new TGeoVolume ("Box1",box1);
-      Box1 -> SetLineColor(kBlack);
-      top -> AddNode(Box1,1,btr);
-
-      
-      TGeoBBox *box2 = new TGeoBBox ("Box2", 64,64,64);
-      TGeoVolume *Box2 = new TGeoVolume ("Box2",box2);
-      Box2 -> SetLineColor(kBlack);
-      Box1 -> AddNode(Box2,1);
-      
-      TGeoBBox *box3 = new TGeoBBox ("Box3", 32,32,32);
-      TGeoVolume *Box3 = new TGeoVolume ("Box3",box3);
-      Box3 -> SetLineColor(kBlack);
-      Box2 -> AddNode(Box3,1);
-      
-      TGeoBBox *box4 = new TGeoBBox ("Box4", 16,16,16);
-      TGeoVolume *Box4 = new TGeoVolume ("Box4",box4);
-      Box4 -> SetLineColor(kBlack);
-      Box3 -> AddNode(Box4,1); 
-      
-      TGeoBBox *box5 = new TGeoBBox ("Box5", 8,8,8);
-      TGeoVolume *Box5 = new TGeoVolume ("Box5",box5);
-      Box5 -> SetLineColor(kBlack);
-      Box4 -> AddNode(Box5,1);
-      
-      TGeoBBox *box6 = new TGeoBBox ("Box6", 4,4,4);
-      TGeoVolume *Box6 = new TGeoVolume ("Box6",box6);
-      Box6 -> SetLineColor(kBlack);
-      Box5 -> AddNode(Box6,1);
-      
-      */
-
     string PN[6]={"x", "y", "u", "v", "yp", "xp"};
     vector<string> PlaneNames(&PN[0],&PN[0]+6);
     // First MWDC
@@ -86,21 +47,6 @@ Detector3D::Detector3D()
     // Second MWDC
      MWDC2 = new WireChamber3D((char*) "MWDC2",  PlaneNames, top, mgr);
 
-
-      //test code: check what transformation : translation and rotation did to a box in top volume
-      
-     /*
-      TGeoBBox *box = new TGeoBBox("Box",5, 5, 5); 
-      TGeoTranslation boxt(0,0,0);
-      TGeoRotation boxr;
-      double ang = 0;
-      boxr.SetAngles(90,0,90-ang,90,ang,-90);
-      TGeoCombiTrans *boxCT= new TGeoCombiTrans(boxt,boxr); 
-      TGeoVolume *Box = new TGeoVolume ("Box", box);
-      Box ->SetLineColor(kBlack);
-      top -> AddNode(Box,1,boxCT);
-      */
-      
 
       // s1x - Scintillation Plane     
       s1xplane = new ScintPlane3D((char*)"s1x",top);
